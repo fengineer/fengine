a78884dc7845cdbd50a00f9fb6a30e2069aa00f0
shufengw <imgamer@163.com>
2017-02-07 17:40:58 +0800

    实现获取系统时间功能GetTimeMS()，单位毫秒。对于Linux而言，是获取1970年到当下的时间，对于windows是获取系统启动以来的时间。注意正确的使用这个时间。

--- fe/src/Lib/Basic/Time.cpp
+++ fe/src/Lib/Basic/Time.cpp
@@ -1,12 +1,56 @@
 
 #include "Time.h"
 
+#include <time.h>
+
+#ifdef _WIN32
+    #include "windows.h"
+#else
+    #include <sys/time.h>
+#endif
+
+
 namespace Fengine
 {
+#ifdef _WIN32
+    class Win32PerformanceFrequency
+    {
+    public:
+        Win32PerformanceFrequency()
+        {
+            QueryPerformanceFrequency((LARGE_INTEGER *)&m_frequency);   // 获取机器固有的性能频率
+            m_frequency /= 1000;    // 每毫秒多少次
+        }
+
+        sint64 GetFrequency()
+        {
+            return m_frequency;
+        }
+    private:
+        sint64 m_frequency;
+    };
+
+    Win32PerformanceFrequency g_win32Frequency;
+#endif
 
 sint64 GetTimeMS()
 {
-	return 0;
+#ifdef _WIN32
+    sint64 tick;
+    QueryPerformanceCounter((LARGE_INTEGER *)&tick);    // 系统启动至今的性能计数
+    return tick / g_win32Frequency.GetFrequency();
+#else
+    struct timeval t;
+    sint64 ms;
+    
+    gettimeofday(&t, 0);
+    ms = t.tv_sec;              // t.tv_sec为int32，复制到sint64后再做乘法运算避免溢出
+    ms *= 1000;                 // 秒转为毫秒
+    ms += (t.tv_usec / 1000);   // 微秒转为毫秒
+
+    return ms;
+#endif
 }
+
 
 }	// end namespace Fingine.
