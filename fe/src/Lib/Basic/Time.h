923a49dd1b4e9a839b12b395949847da52b37311
shufengw <imgamer@163.com>
2017-02-08 09:26:20 +0800

    add the GNU Lesser General Public License comments

--- fe/src/Lib/Basic/Time.h
+++ fe/src/Lib/Basic/Time.h
@@ -1,3 +1,22 @@
+/*
+This source file is part of Fengine
+For the latest info, see 
+
+Copyright (c) 2017 Fengine.
+
+Fengine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Fengine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Fengine.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef __TIME_H__
 #define __TIME_H__
