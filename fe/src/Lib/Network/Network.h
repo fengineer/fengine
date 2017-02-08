972bb420a2b39922332694e383d16ba0c79e8e25
shufengw <imgamer@163.com>
2017-02-08 13:44:10 +0800

    1. 建立网络模块结构；2. 建立测试项目vs工程；

---
+++ fe/src/Lib/Network/Network.h
@@ -0,0 +1,27 @@
+/*
+This source file is part of Fengine
+For the latest info, see
+
+Copyright (c) 2017 Fengine.
+
+Fengine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Fengine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Fengine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __NETWORK_H__
+#define __NETWORK_H__
+
+#include "Socket.h"
+
+
+#endif // __NETWORK_H__
